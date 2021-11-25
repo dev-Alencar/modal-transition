dab007ba35144e5c5f89b40bd5dbbd014320f541
dev-Alencar <lucalencar.dev@gmail.com>
2021-11-25 18:05:47 -0300

    modal-lógica

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,138 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="modalAtivo = !modalAtivo">Cadastre-se</button>
+
+    <transition name="modal"> 
+        <div class="modal-wrapper" v-show="modalAtivo" @click="cliqueFora" >
+            <div class="modal"> 
+                <h1>Cadastre-se</h1>
+                <form>
+                  <label for="emaail">E-mail</label>
+                  <input type="email" name="email" id="email">
+
+                   <label for="password">Senha</label>
+                  <input type="password" name="password" id="password">
+
+                  <input type="submit" value="Logar">
+                </form>
+            </div>
+        </div>  
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            modalAtivo: false
+        }
+    },
+    methods: {
+      cliqueFora( event ) {
+        if (event.currentTarget === event.target) {
+          return this.modalAtivo = false
+        }
+        
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  * {
+    box-sizing: border-box;
+  }   
+
+  body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  h1 {
+    margin: 0px 0px 20px 0px ;
+    
+  }
+
+  .modal-wrapper {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0px;
+    left: 0px;
+    background: rgba(0,0,0,0.4);
+
+    overflow-y: scroll; /* Isso da possibilidade de dar scroll quando não tiver dando pra ler o modal. */
+  }
+
+  .modal {
+    background: #e5e5e5;
+    padding: 20px;
+    border-radius: 4px;
+    max-width: 400px;
+    margin: 120px auto 0 auto ;
+    z-index: 10;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  input, button {
+    width: 100%;
+    border:  2px solid rgba(0,0,0,0.2);
+    border-radius: 2px ;
+    padding: 15px 10px;
+    font-size: 1rem;
+    margin-bottom: 20px;
+  }
+
+ input[type="submit"], 
+ button {
+   width: 200px;
+   background: #84e;
+   color: white;
+   text-transform: uppercase;
+   font-size: 1rem;
+   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+ }
+
+ input[type="submit"]:active,
+ button:active {
+   background: #94e;
+ }
+
+ button {
+   margin: 20px auto ;
+   display: block;
+   cursor: pointer;
+ }
+
+  .modal-enter
+  .modal-leave-to {
+    opacity: 0;
+  }
+
+  .modal-enter-to .modal {
+    animation: slide 0.5s;
+  }
+
+  .modal-leave-to .modal {
+    animation: slide 0.5s reverse;
+  }
+
+  @keyframes slide {
+    from {
+      transform: translate3d( 0, -40px, 0 )
+    } to {
+      transform: translate3d(0, 0px, 0);
+    }
+
+  }
+
+  .modal-enter-active,
+  .modal-leave-to {
+    transition: opacity 0.5s
+  }
 </style>
